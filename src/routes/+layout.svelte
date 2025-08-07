b2b5182f16aa7e1fd78f5b67654a8b30ba78aafb
Kittiphat S <kittiphat@muze.co.th>
2025-08-07 10:25:48 +0700

    feat: init web-page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import '../app.css'
+  import favicon from '$lib/assets/favicon.svg'
+  import Footer from '$lib/components/footer.svelte'
+  import NavBar from '$lib/components/nav-bar.svelte'
+
+  let { children } = $props()
+</script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
+
+<NavBar />
+<div class="min-h-screen pt-[60px]">
+  {@render children?.()}
+  <Footer />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import SearchBar from '$lib/components/search-bar.svelte'
+</script>
+
+<div class="p-6">
+  <div class="text-center">
+    <SearchBar />
+  </div>
+
+  <button class="btn">Button</button>
+</div>
